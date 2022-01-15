2f1efd124b79bd59d97b7d45fe927252d829ea65
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2022-01-15 15:15:10 -0500

    Add sync script

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+rsync -avhP --exclude='node_modules' \
+    --exclude='.git'  \
+    --exclude='.DS_Store'  \
+    --exclude='node_modules'  \
+    --exclude='build'  \
+    --exclude='package-lock.json'  \
+    --exclude='.vscode'  \
+    --exclude='dist'  \
+    . matrix:~/matrix-dash
