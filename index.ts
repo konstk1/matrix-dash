d31136337b86d9b9c7bda7d317368e82341ec4ad
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2022-01-15 15:01:34 -0500

    Add led matrix package

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log('Typescript test');
+import { LedMatrix, GpioMapping } from 'rpi-led-matrix';
+
+const matrix = new LedMatrix({
+  ...LedMatrix.defaultMatrixOptions(),
+  rows: 32,
+  cols: 64,
+  hardwareMapping: GpioMapping.AdafruitHat,
+  // pixelMapper: PixelMapperType.Simple,
+}, {
+  ...LedMatrix.defaultRuntimeOptions(),
+  gpioSlowdown: 2,
+});
+
+console.log('Writing screen...');
+
+matrix
+  .clear()
+  .brightness(100)
+  .fgColor(0x0000FF)
+  .fill()
+
+console.log('Done');
