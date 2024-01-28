a37976976ff49ed628dba1d354a3cdd24f6fe4c3
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2024-01-28 14:53:14 -0500

    Add carousel widget unit tests

--- test/carousel.test.ts
+++ test/carousel.test.ts
@@ -3,6 +3,12 @@ import { CarouselWidget } from '../src/widgets/carousel-widget'
 import { Widget } from '../src/widgets/widget'
 
 class TestWidget extends Widget {
+  public fireEvent() {
+
+  }
+
+  public endEvent() {
+  }
 }
 
 jest.useFakeTimers()
@@ -10,8 +16,9 @@ jest.useFakeTimers()
 describe('Carousel', () => {
   const widget1 = new TestWidget({ width: 10, height: 10 })
   const widget2 = new TestWidget({ width: 10, height: 10 })
+  const widget3 = new TestWidget({ width: 10, height: 10 })
 
-  test('switches widgets on timer', () => {
+  test.only('switches widgets on timer', () => {
     const carousel = new CarouselWidget({ width: 10, height: 10 })
     expect(carousel.numWidgets()).toBe(0)
 
@@ -23,10 +30,55 @@ describe('Carousel', () => {
     carousel.activate()
     expect(carousel.activeWidget()).toBe(widget1)
 
-    jest.runOnlyPendingTimers()
+    jest.advanceTimersByTime(5000)
+    expect(carousel.activeWidget()).toBe(widget2)
+
+    jest.advanceTimersByTime(5000)
+    expect(carousel.activeWidget()).toBe(widget1)
+  })
+
+  test('switches between highest priority widgets only', () => {
+    const carousel = new CarouselWidget({ width: 10, height: 10 })
+
+    carousel.addWidget(widget1, { displayTimeSec: 5, defaultPriority: 0, activePriority: 0 })
+    carousel.addWidget(widget2, { displayTimeSec: 5, defaultPriority: 1, activePriority: 1 })
+    carousel.addWidget(widget3, { displayTimeSec: 5, defaultPriority: 1, activePriority: 1 })
+
+    expect(carousel.numWidgets()).toBe(2)
+
+    carousel.activate()
     expect(carousel.activeWidget()).toBe(widget2)
 
     jest.runOnlyPendingTimers()
+    expect(carousel.activeWidget()).toBe(widget3)
+
+    jest.runOnlyPendingTimers()
+    expect(carousel.activeWidget()).toBe(widget2)
+  })
+
+  test('switches to widget with event', () => {
+    const carousel = new CarouselWidget({ width: 10, height: 10 })
+
+    carousel.addWidget(widget1, { displayTimeSec: 5, defaultPriority: 0, activePriority: 0 })
+    carousel.addWidget(widget2, { displayTimeSec: 5, defaultPriority: 0, activePriority: 5 })
+    carousel.addWidget(widget3, { displayTimeSec: 5, defaultPriority: 0, activePriority: 5 })
+
+    carousel.activate()
+    expect(carousel.activeWidget()).toBe(widget1)
+
+    widget2.fireEvent()
+    expect(carousel.activeWidget()).toBe(widget2)
+
+    jest.runOnlyPendingTimers()
+    expect(carousel.activeWidget()).toBe(widget2)
+
+    widget3.fireEvent()
+    expect(carousel.activeWidget()).toBe(widget3)
+
+    widget2.endEvent()
+    expect(carousel.activeWidget()).toBe(widget3)
+
+    widget3.endEvent()
     expect(carousel.activeWidget()).toBe(widget1)
   })
 })
