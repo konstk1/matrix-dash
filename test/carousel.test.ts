9a0a0caf6d5f91b4689bfa82ec27e8151f514513
Konstantin Klitenik <5853429+konstk1@users.noreply.github.com>
2024-03-31 10:26:07 -0400

    Start widget event work

--- test/carousel.test.ts
+++ test/carousel.test.ts
@@ -18,7 +18,7 @@ describe('Carousel', () => {
   const widget2 = new TestWidget({ width: 10, height: 10 })
   const widget3 = new TestWidget({ width: 10, height: 10 })
 
-  test.only('switches widgets on timer', () => {
+  test('switches widgets on timer', () => {
     const carousel = new CarouselWidget({ width: 10, height: 10 })
     expect(carousel.numWidgets()).toBe(0)
 
@@ -50,7 +50,7 @@ describe('Carousel', () => {
     carousel.addWidget(widget2, { displayTimeSec: 5, defaultPriority: 1, activePriority: 1 })
     carousel.addWidget(widget3, { displayTimeSec: 5, defaultPriority: 1, activePriority: 1 })
 
-    expect(carousel.numWidgets()).toBe(2)
+    expect(carousel.numWidgets()).toBe(3)
 
     carousel.activate()
     expect(carousel.activeWidget()).toBe(widget2)
@@ -65,26 +65,24 @@ describe('Carousel', () => {
   test('switches to widget with event', () => {
     const carousel = new CarouselWidget({ width: 10, height: 10 })
 
-    carousel.addWidget(widget1, { displayTimeSec: 5, defaultPriority: 0, activePriority: 0 })
-    carousel.addWidget(widget2, { displayTimeSec: 5, defaultPriority: 0, activePriority: 5 })
-    carousel.addWidget(widget3, { displayTimeSec: 5, defaultPriority: 0, activePriority: 5 })
+    carousel.addWidget(widget1, { displayTimeSec: 0, defaultPriority: 0, activePriority: 0 })
+    carousel.addWidget(widget2, { displayTimeSec: 0, defaultPriority: 0, activePriority: 5 })
 
     carousel.activate()
+    expect(carousel.activeWidget()).toBe(widget1)
+
+    // expect widget 1 to be displayed indefinitely
+    jest.advanceTimersByTime(100000)
     expect(carousel.activeWidget()).toBe(widget1)
 
     widget2.fireEvent()
     expect(carousel.activeWidget()).toBe(widget2)
 
-    jest.runOnlyPendingTimers()
+    // expect widget 2 to be displayed until event is done
+    jest.advanceTimersByTime(100000)
     expect(carousel.activeWidget()).toBe(widget2)
 
-    widget3.fireEvent()
-    expect(carousel.activeWidget()).toBe(widget3)
-
     widget2.endEvent()
-    expect(carousel.activeWidget()).toBe(widget3)
-
-    widget3.endEvent()
     expect(carousel.activeWidget()).toBe(widget1)
   })
 })
